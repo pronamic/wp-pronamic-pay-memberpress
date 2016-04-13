56eb0533b0b3c849451bea7eeb4f71d4282e4dae
Remco Tolsma <info@remcotolsma.nl>
2016-04-13 10:11:03 +0200

    Simplified the transaction has status function, else is never required.

--- src/MemberPress.php
+++ src/MemberPress.php
@@ -12,20 +12,16 @@
  */
 class Pronamic_WP_Pay_Extensions_MemberPress_MemberPress {
 	/**
-	 * Transaction has status
+	 * Transaction has status.
 	 *
 	 * @param MeprTransaction $transaction
 	 * @param string|array $status
 	 */
 	public static function transaction_has_status( $transaction, $status ) {
-		$has_status = false;
-
 		if ( is_array( $status ) ) {
-			$has_status = in_array( $transaction->status, $status, true );
-		} else {
-			$has_status = ( $transaction->status === $status );
+			return in_array( $transaction->status, $status, true );
 		}
 
-		return $has_status;
+		return ( $transaction->status === $status );
 	}
 }
