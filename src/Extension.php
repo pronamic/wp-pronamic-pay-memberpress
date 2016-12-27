aec90fa08f2a7c704650ce861909505f58781ee1
Remco Tolsma <info@remcotolsma.nl>
2016-12-27 13:54:45 +0100

    Added filter payment source description and URL.

--- src/Extension.php
+++ src/Extension.php
@@ -38,6 +38,8 @@ class Pronamic_WP_Pay_Extensions_MemberPress_Extension {
 		add_action( 'pronamic_payment_status_update_' . self::SLUG, array( __CLASS__, 'status_update' ), 10, 1 );
 
 		add_filter( 'pronamic_payment_source_text_' . self::SLUG,   array( __CLASS__, 'source_text' ), 10, 2 );
+		add_filter( 'pronamic_payment_source_description_' . self::SLUG,   array( __CLASS__, 'source_description' ), 10, 2 );
+		add_filter( 'pronamic_payment_source_url_' . self::SLUG,   array( __CLASS__, 'source_url' ), 10, 2 );
 	}
 
 	/**
@@ -167,5 +169,27 @@ class Pronamic_WP_Pay_Extensions_MemberPress_Extension {
 		);
 
 		return $text;
+	}
+
+	/**
+	 * Source description.
+	 */
+	public static function source_description( $description, Pronamic_Pay_Payment $payment ) {
+		$description = __( 'MemberPress Transaction', 'pronamic_ideal' );
+
+		return $description;
+	}
+
+	/**
+	 * Source URL.
+	 */
+	public static function source_url( $url, Pronamic_Pay_Payment $payment ) {
+		$url = add_query_arg( array(
+			'page'   => 'memberpress-trans',
+			'action' => 'edit',
+			'id'     => $payment->source_id,
+		), admin_url( 'admin.php' ) );
+
+		return $url;
 	}
 }
