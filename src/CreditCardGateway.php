79c5354ded0b4cc0c54a252ce8262df68c466b88
Remco Tolsma <info@remcotolsma.nl>
2017-01-25 14:31:11 +0100

    Getting ready for version 1.0.4.

--- src/CreditCardGateway.php
+++ src/CreditCardGateway.php
@@ -3,11 +3,11 @@
 /**
  * Title: WordPress pay MemberPress credit card gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.4
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_MemberPress_CreditCardGateway extends Pronamic_WP_Pay_Extensions_MemberPress_Gateway {
--- src/Extension.php
+++ src/Extension.php
@@ -3,11 +3,11 @@
 /**
  * Title: WordPress pay MemberPress extension
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.3
+ * @version 1.0.4
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_MemberPress_Extension {
