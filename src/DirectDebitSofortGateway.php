f1cd310a39a39190337cbe2db64e796d90504060
Reüel van der Steege <reuel@pronamic.nl>
2017-12-22 12:26:58 +0100

    Add `Direct Debit (mandate via Sofort)` payment method.

--- src/DirectDebitSofortGateway.php
+++ src/DirectDebitSofortGateway.php
@@ -1,32 +1,32 @@
 <?php
 
 /**
- * Title: WordPress pay MemberPress Direct Debit mandate via iDEAL gateway
+ * Title: WordPress pay MemberPress Direct Debit mandate via Sofort gateway
  * Description:
  * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Reüel van der Steege
- * @version 1.0.0
- * @since unreleased
+ * @version 1.0.6
+ * @since 1.0.6
  */
-class Pronamic_WP_Pay_Extensions_MemberPress_DirectDebitIDealGateway extends Pronamic_WP_Pay_Extensions_MemberPress_Gateway {
+class Pronamic_WP_Pay_Extensions_MemberPress_DirectDebitSofortGateway extends Pronamic_WP_Pay_Extensions_MemberPress_Gateway {
 	/**
-	 * Constructs and initialize Direct Debit mandate via iDEAL gateway.
+	 * Constructs and initialize Direct Debit mandate via Sofort gateway.
 	 */
 	public function __construct() {
 		parent::__construct();
 
 		// Set the name of this gateway.
 		// @see https://gitlab.com/pronamic/memberpress/blob/1.2.4/app/lib/MeprBaseGateway.php#L12-13
-		$this->name           = __( 'Direct Debit (mandate via iDEAL)', 'pronamic_ideal' );
-		$this->payment_method = Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT_IDEAL;
+		$this->name           = __( 'Direct Debit (mandate via Sofort)', 'pronamic_ideal' );
+		$this->payment_method = Pronamic_WP_Pay_PaymentMethods::DIRECT_DEBIT_SOFORT;
 	}
 
 	/**
 	 * Get icon function, please note that this is not a MemberPress function.
 	 *
-	 * @since 1.0.2
+	 * @since unreleased
 	 * @return string
 	 */
 	protected function get_icon() {
@@ -39,6 +39,6 @@ class Pronamic_WP_Pay_Extensions_MemberPress_DirectDebitIDealGateway extends Pro
 	 * @return string
 	 */
 	public function get_alias() {
-		return 'MeprDirectDebitIDealGateway';
+		return 'MeprDirectDebitSofortGateway';
 	}
 }
