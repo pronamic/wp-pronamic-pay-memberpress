307887d8227713e0d4145844bf608c86e3165a2d
Reüel van der Steege <reuel@pronamic.nl>
2018-01-03 10:55:16 +0100

    Happy 2018.

--- src/DirectDebitIDealGateway.php
+++ src/DirectDebitIDealGateway.php
@@ -3,7 +3,7 @@
 /**
  * Title: WordPress pay MemberPress Direct Debit mandate via iDEAL gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Reüel van der Steege
--- src/MisterCashGateway.php
+++ src/MisterCashGateway.php
@@ -3,7 +3,7 @@
 /**
  * Title: WordPress pay MemberPress Bancontact/Mister Cash gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
