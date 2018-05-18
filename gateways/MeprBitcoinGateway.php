7681fb43e4f9e001041a927c6b78725943f6e0a0
Remco Tolsma <info@remcotolsma.nl>
2018-05-18 11:26:46 +0200

    Improving phpcs.

--- gateways/MeprBitcoinGateway.php
+++ gateways/MeprBitcoinGateway.php
@@ -1,3 +1,11 @@
 <?php
+/**
+ * Bitcoin gateway alias
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Extensions\MemberPress
+ */
 
 class_alias( 'Pronamic\WordPress\Pay\Extensions\MemberPress\BitcoinGateway', 'MeprBitcoinGateway' );
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Bootstrap tests
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2018 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Extensions\MemberPress
+ */
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
