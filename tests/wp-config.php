1c8ae072b0a6b7196b6bdaf9b9b6adf44d2643bd
Remco Tolsma <info@remcotolsma.nl>
2018-06-01 14:46:39 +0200

    Revert "Try to fix Travis CI errors."
    This reverts commit 517ce2447552820c3bec31aab59606bed60bb72b.

--- tests/wp-config.php
+++ tests/wp-config.php
@@ -1,5 +1,8 @@
 <?php
 
+/* Path to the WordPress codebase you'd like to test. Add a forward slash in the end. */
+define( 'ABSPATH', dirname( dirname( __FILE__ ) ) . '/wordpress/' );
+
 /*
  * Path to the theme to test with.
  *
