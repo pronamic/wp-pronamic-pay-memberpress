a22b6066c46b6a866231abcf84a9946cde5f6a8c
Reüel van der Steege <reuel@pronamic.nl>
2018-06-19 09:50:39 +0200

    Use renamed Item methods in payment data.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -135,10 +135,10 @@ class PaymentData extends Pay_PaymentData {
 		$items = new Items();
 
 		$item = new Item();
-		$item->setNumber( $this->get_order_id() );
-		$item->setDescription( $this->get_description() );
-		$item->setPrice( $this->transaction->total );
-		$item->setQuantity( 1 );
+		$item->set_number( $this->get_order_id() );
+		$item->set_description( $this->get_description() );
+		$item->set_price( $this->transaction->total );
+		$item->set_quantity( 1 );
 
 		$items->addItem( $item );
 
