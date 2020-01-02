e6e92fa2e52e80d6943d947c8306179828abbe08
Reüel van der Steege <reuel@pronamic.nl>
2020-01-02 14:32:05 +0100

    Happy 2020.

--- gateways/MeprDirectDebitIDealGateway.php
+++ gateways/MeprDirectDebitIDealGateway.php
@@ -3,7 +3,7 @@
  * Direct Debit mandate via iDEAL gateway alias
  *
  * @author    Pronamic <info@pronamic.eu>
- * @copyright 2005-2019 Pronamic
+ * @copyright 2005-2020 Pronamic
  * @license   GPL-3.0-or-later
  * @package   Pronamic\WordPress\Pay\Extensions\MemberPress
  */
